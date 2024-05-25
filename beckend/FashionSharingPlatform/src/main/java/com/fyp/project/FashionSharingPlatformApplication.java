96bde50403884d6b51252e37b276dee318365d62
Hanan Shaikh <hannanshaikh150@gmail.com>
2024-05-25 17:14:48 +0500

    setup the project and the login form

---
+++ beckend/FashionSharingPlatform/src/main/java/com/fyp/project/FashionSharingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionSharingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionSharingPlatformApplication.class, args);
+	}
+
+}
